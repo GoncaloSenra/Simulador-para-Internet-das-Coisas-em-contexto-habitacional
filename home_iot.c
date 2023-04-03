efb4b22b65a920611bd165f4395d2d63aeb296f7
Gonçalo Senra <goncalosenra@gmail.com>
2023-04-03 20:06:09 +0100

    Log file fixed

--- home_iot.c
+++ home_iot.c
@@ -17,8 +17,18 @@
 #include <pthread.h>
 #include <time.h>
 
+typedef struct shared_memory{
+    int N_WORKERS;
+    int QUEUE_SZ;
+    int MAX_ALERTS;
+    int MAX_KEYS;
+    int MAX_SENSORS;
+    int teste;
 
-int * shared_var;
+} Shared_var;
+
+
+Shared_var* sh_var;
 int shmid;
 sem_t *mutex_shm;
 sem_t *mutex_log;
@@ -37,28 +47,41 @@ void write_logfile(char * message){
     sprintf(time_aux, "%d:%d:%d ", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
 
     sem_wait(mutex_log);
-
+    
+    log = fopen("log.txt", "a");
+    
     fwrite(time_aux, 1, strlen(time_aux), log);
     fwrite(message, 1, strlen(message), log);
     printf("%s", time_aux);
     printf("%s", message);
-
+    
+    fclose(log);
+    
     sem_post(mutex_log);
 
     free(time_aux);
 
 }
 
-int worker(pid_t pid) {
+int worker(int id) {
 
-    printf("WORKER[%d] STARTED!\n", pid);
+    char* mes = malloc(sizeof(char) * (20));
+    sprintf(mes, "WORKER %d READY\n", id);
+    write_logfile(mes);
+    free(mes);
+
+    sem_wait(mutex_shm);
+	sh_var->teste++;
+    printf("TESTE: %d\n", sh_var->teste);
+    sem_post(mutex_shm);
 
     return 0;
 }
 
-int alerts_watcher(pid_t pid) {
+int alerts_watcher(int id) {
 
-    printf("ALERTS_WATCHER[%d] STARTED!\n", pid);
+    write_logfile("PROCESS ALERTS_WATCHER CREATED\n");
+
 
     return 0;
 }
@@ -66,16 +89,6 @@ int alerts_watcher(pid_t pid) {
 void * console_reader(void * id_t) {
 
     write_logfile("THREAD CONSOLE_READER CREATED\n");
-
-    //int id = *((int *)id_t);
-    //pthread_t idt=pthread_self();
-
-    //printf("THREAD[%ld]: CONSOLE READER[%d] STARTED!\n", idt, id);
-
-    sem_wait(mutex_shm);
-	(*shared_var)++;
-    printf("CONSOLE READER: %d\n", *shared_var);
-    sem_post(mutex_shm);
 
 
     pthread_exit(NULL);
@@ -85,15 +98,6 @@ void * sensor_reader(void * id_t) {
 
     write_logfile("THREAD SENSOR_READER CREATED\n");
 
-    //int id = *((int *)id_t);
-    //pthread_t idt=pthread_self();
-
-    //printf("THREAD[%ld]: SENSOR READER[%d] STARTED!\n", idt, id);
-
-    sem_wait(mutex_shm);
-	(*shared_var)++;
-    printf("SENSOR READER: %d\n", *shared_var);
-    sem_post(mutex_shm);
 
     pthread_exit(NULL);
 }
@@ -102,109 +106,29 @@ void * dispatcher(void * id_t) {
 
     write_logfile("THREAD DISPATCHER CREATED\n");
 
-    //int id = *((int *)id_t);
-    //pthread_t idt=pthread_self();
-
-    //printf("THREAD[%ld]: DISPATCHER[%d] STARTED!\n", idt, id);
-
-    sem_wait(mutex_shm);
-	(*shared_var)++;
-    printf("DISPATCHER: %d\n", *shared_var);
-    sem_post(mutex_shm);
-
 
     pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[]) {
+
+int system_manager(pid_t pid) {
 
     pthread_t console_reader_t;
     pthread_t sensor_reader_t;
     pthread_t dispatcher_t;
 
-    log = fopen("log.txt", "w");
-
-
-    if (argc != 2) {
-        printf("Error!\n");
-        return -1;
-    }
-
-    FILE * config = fopen(argv[1], "r");
-    char line[30];
-
-    fgets(line, 30, config);
-    int QUEUE_SZ = atoi(line);
-
-    if (QUEUE_SZ < 1) {
-        printf("QUEUE_SZ must be (>=1)\n");
-        return -1;
-    }
-
-    fgets(line, 30, config);
-    int N_WORKERS = atoi(line);
-
-    if (N_WORKERS < 1) {
-        printf("N_WORKERS must be (>=1)\n");
-        return -1;
-    }
-
-    fgets(line, 30, config);
-    int MAX_KEYS = atoi(line);
-
-    if (MAX_KEYS < 1) {
-        printf("MAX_KEYS must be (>=1)\n");
-        return -1;
-    }
-
-    fgets(line, 30, config);
-    int MAX_SENSORS = atoi(line);
-
-    if (MAX_SENSORS < 1) {
-        printf("MAX_SENSORS must be (>=1)\n");
-        return -1;
-    }
-
-    fgets(line, 30, config);
-    int MAX_ALERTS = atoi(line);
-
-    if (MAX_ALERTS < 0) {
-        printf("MAX_ALERTS must be (>=0)\n");
-        return -1;
-    }
-
-    fclose(config);
-
-    // Create Shared Memory
-    shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT|0777);
-
-    // Attach shared memory
-	shared_var = (int *) shmat(shmid, NULL, 0);
-
-    // Create Semaphores
-    sem_unlink("MUTEX_SHM");
-	mutex_shm = sem_open("MUTEX_SHM", O_CREAT|O_EXCL, 0777, 1);
-    sem_unlink("MUTEX_LOG");
-	mutex_log = sem_open("MUTEX_LOG", O_CREAT|O_EXCL, 0777, 1);
-
-    write_logfile("HOME_IOT SIMULATOR STARTING\n");
-
     // Create Workers
     int i;
-    for (i = 0; i < N_WORKERS; i++) {
+    for (i = 0; i < sh_var->N_WORKERS; i++) {
         if (fork() == 0) {
-            char * mes = NULL;
-            sprintf(mes, "WORKER %d READY\n", i+1);
-            write_logfile(mes);
-            worker(getpid());
+            worker(i + 1);
             exit(0);
         }
     }
 
     // Create Alerts_Watcher
     if (fork() == 0) {
-        write_logfile("PROCESS ALERTS_WATCHER CREATED\n");
-        alerts_watcher(getpid());
+        alerts_watcher(0);
         exit(0);
     }
 
@@ -220,20 +144,111 @@ int main(int argc, char *argv[]) {
     int d_id = 3;
     pthread_create(&dispatcher_t, NULL, dispatcher, &d_id);
 
-
-    //write_logfile("HOME_IOT SIMULATOR WAITING FOR LAST TASKS TO FINISH\n");
-
     // Join Threads
     pthread_join(console_reader_t, NULL);
     pthread_join(sensor_reader_t, NULL);
     pthread_join(dispatcher_t, NULL);
 
 
-
     // Wait for Workers and Alerts_Watcher
-    for (i = 0; i < N_WORKERS + 1; i++) {
+    for (i = 0; i < sh_var->N_WORKERS + 1; i++) {
         wait(NULL);
     }
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+
+    
+    log = fopen("log.txt", "w");
+    fclose(log);
+
+    if (argc != 2) {
+        printf("Error!\n");
+        return -1;
+    }
+
+    // Create Shared Memory
+    shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT|0777);
+
+    // Attach shared memory
+	sh_var = (Shared_var *) shmat(shmid, NULL, 0);
+
+
+    FILE * config = fopen(argv[1], "r");
+    char line[30];
+
+    fgets(line, 30, config);
+    int QUEUE_SZ = atoi(line);
+
+    if (QUEUE_SZ < 1) {
+        printf("QUEUE_SZ must be (>=1)\n");
+        return -1;
+    }
+
+    sh_var->QUEUE_SZ = QUEUE_SZ;
+
+    fgets(line, 30, config);
+    int N_WORKERS = atoi(line);
+
+    if (N_WORKERS < 1) {
+        printf("N_WORKERS must be (>=1)\n");
+        return -1;
+    }
+    
+    sh_var->N_WORKERS = N_WORKERS;
+
+    fgets(line, 30, config);
+    int MAX_KEYS = atoi(line);
+
+    if (MAX_KEYS < 1) {
+        printf("MAX_KEYS must be (>=1)\n");
+        return -1;
+    }
+
+    sh_var->MAX_KEYS = MAX_KEYS;
+
+    fgets(line, 30, config);
+    int MAX_SENSORS = atoi(line);
+
+    if (MAX_SENSORS < 1) {
+        printf("MAX_SENSORS must be (>=1)\n");
+        return -1;
+    }
+
+    sh_var->MAX_SENSORS = MAX_SENSORS;
+
+    fgets(line, 30, config);
+    int MAX_ALERTS = atoi(line);
+
+    if (MAX_ALERTS < 0) {
+        printf("MAX_ALERTS must be (>=0)\n");
+        return -1;
+    }
+
+    sh_var->MAX_ALERTS = MAX_ALERTS;
+
+    fclose(config);
+
+    sh_var->teste = 0;
+
+    // Create Semaphores
+    sem_unlink("MUTEX_SHM");
+	mutex_shm = sem_open("MUTEX_SHM", O_CREAT|O_EXCL, 0777, 1);
+    sem_unlink("MUTEX_LOG");
+	mutex_log = sem_open("MUTEX_LOG", O_CREAT|O_EXCL, 0777, 1);
+
+
+    pid_t sys_manager = fork();
+    if (sys_manager > 0) {
+        write_logfile("HOME_IOT SIMULATOR STARTING\n");
+    } else if (sys_manager == 0){
+        system_manager(getpid());
+        exit(0);
+    }
+	
+    wait(NULL);
 
     write_logfile("HOME_IOT SIMULATOR CLOSING\n");
 
@@ -244,10 +259,8 @@ int main(int argc, char *argv[]) {
     sem_close(mutex_log);
 	sem_unlink("MUTEX_LOG");
 	
-	shmdt(shared_var);
-	shmctl(shmid, IPC_RMID, NULL);
-
-
+	//shmdt(sh_var);
+	//shmctl(shmid, IPC_RMID, NULL);
 
     return 0;
 }
